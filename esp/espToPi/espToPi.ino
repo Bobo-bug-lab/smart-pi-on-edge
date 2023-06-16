c28754010c810a63483b9d097f14f28627465071
Bobo-bug-lab <645498374@qq.com>
2023-06-17 00:31:02 +0200

    added cv_model and node-red-on-edge

--- esp/espToPi/espToPi.ino
+++ esp/espToPi/espToPi.ino
@@ -1,14 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include "Light_sensor.h"
+
+// Define pins for sensors
+#define PIN_PIR D2
+#define PIN_LIGHT_SENSOR A0
+
+static Light_sensor* light_sensor;
+static int light_value=0,detect=0;
 
 // WiFi参数
-const char* ssid = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
+const char* ssid = "Vodafone-63Hao";
+const char* password = "liushisan.4";
 
 // MQTT参数
-const char* mqtt_server = "YOUR_MQTT_BROKER_IP";
-const int mqtt_port = 1883;
-const char* mqtt_topic = "YOUR_MQTT_TOPIC";
+const char* mqtt_server = "192.168.1.17";
+const int mqtt_port = 1888;
+const char* mqtt_topic = "esp";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -25,6 +33,9 @@ void setup() {
   Serial.println("");
   Serial.print("WiFi connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  light_sensor = new Light_sensor(PIN_LIGHT_SENSOR);
+  pinMode(PIN_PIR, INPUT_PULLUP);
 
   // 设置MQTT服务器和回调函数
   client.setServer(mqtt_server, mqtt_port);
@@ -44,9 +55,16 @@ void loop() {
   client.loop();
 
   // 发送传感器数据
-  float sensorValue = analogRead(A0) * 0.0032258;  // 读取传感器数据
-  char payload[10];
-  dtostrf(sensorValue, 4, 2, payload);  // 转换为字符串
+   light_value = light_sensor->getLightLevel();
+   detect = digitalRead(PIN_PIR);
+   Serial.println("detect_status: ");
+   Serial.println(detect);
+   Serial.println("light_value: ");
+   Serial.println(light_value);
+
+
+  char payload[20];
+  sprintf(payload, "{\"light\": %d, \"detect\": %d}", light_value, detect);
   client.publish(mqtt_topic, payload);  // 发布消息到MQTT主题
   delay(5000);
 }
@@ -70,7 +88,7 @@ void reconnect() {
     Serial.print("Attempting MQTT connection...");
     
     // 生成客户端ID（根据需要修改）
-    String clientId = "ESP8266Client-";
+    String clientId = "ESP8266Client";
     clientId += String(random(0xffff), HEX);
 
     // 尝试连接
